18d53a9729f6f912babfcc043d80e99cc0b490a5
Luca Bellenghi <lucabel@gmail.com>
2014-05-08 10:13:02 +0200

    First commit

---
+++ cpskin/slider/__init__.py
@@ -0,0 +1,7 @@
+# -*- extra stuff goes here -*-
+from zope.i18n import MessageFactory
+
+messageFactory = MessageFactory("cpskin.slider")
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
